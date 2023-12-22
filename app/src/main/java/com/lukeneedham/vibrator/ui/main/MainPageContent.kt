23b22f43774edc30a9a5483a146823cce143fa1d
Luke <lukeneedham@outlook.com>
2023-12-22 23:25:49 +0100

    add icon and move strings to xml

--- app/src/main/java/com/lukeneedham/vibrator/ui/main/MainPageContent.kt
+++ app/src/main/java/com/lukeneedham/vibrator/ui/main/MainPageContent.kt
@@ -10,8 +10,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import com.lukeneedham.vibrator.R
 
 @Composable
 fun MainPageContent(startVibration: () -> Unit, stopVibration: () -> Unit) {
@@ -34,12 +36,12 @@ fun MainPageContent(startVibration: () -> Unit, stopVibration: () -> Unit) {
         }
 
         Button(
-            text = "Start vibration",
+            text = stringResource(id = R.string.vibration_start),
             color = startColor,
             onClick = startVibration,
         )
         Button(
-            text = "Stop vibration",
+            text = stringResource(id = R.string.vibration_stop),
             color = stopColor,
             onClick = stopVibration,
         )
