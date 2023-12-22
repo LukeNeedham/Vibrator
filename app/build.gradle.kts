734ff02dd0cadaa4efa0bbe8142d30f650966f7f
Luke <lukeneedham@outlook.com>
2023-12-22 23:39:58 +0100

    Add logo images to repo. Use material instead of material3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,16 +51,17 @@ android {
 
 dependencies {
     /* Misc */
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
 
     /* Compose */
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
 
     /* Testing */
     testImplementation("junit:junit:4.13.2")
