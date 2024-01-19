8fc2c1a060745f5787d1506291ddc5c73ee19254
Luke <lukeneedham@outlook.com>
2024-01-19 19:50:30 +0100

    Remove tests, remove superfluous dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,27 +50,11 @@ android {
 }
 
 dependencies {
-    /* Misc */
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    /* Activity */
     implementation("androidx.activity:activity-compose:1.8.2")
 
     /* Compose */
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
     implementation("androidx.compose.material:material")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-
-    /* Testing */
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-
-    /* Tooling */
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
--- app/src/main/java/com/lukeneedham/vibrator/ui/main/MainPage.kt
+++ app/src/main/java/com/lukeneedham/vibrator/ui/main/MainPage.kt
@@ -1,11 +1,6 @@
 package com.lukeneedham.vibrator.ui.main
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MainPage(
