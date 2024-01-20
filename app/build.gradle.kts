96b8e8c079c6e3a961c4e95b38fd3d700163ac1c
Luke <lukeneedham@outlook.com>
2024-01-20 23:02:57 +0100

    Bump version to 1.0.2 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.lukeneedham.vibrator"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
